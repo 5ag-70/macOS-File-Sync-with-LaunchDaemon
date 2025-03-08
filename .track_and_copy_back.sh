b6803f96298c95ed7788b38fe0f764e558fbc6aa
Sagar Rana <sagarrana@Sagars-MacBook-Pro-16.local>
2025-03-08 11:19:04 +0000

    Changes to the correct dir and added chown to sync with icloud and changed the log message.

--- .track_and_copy_back.sh
+++ .track_and_copy_back.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+
 # Source and destination paths
-ZPROFILE_SOURCE="/Users/sagarrana/Documents/Kaseya_System/.zprofile"
-ZPROFILE_DEST="/Users/sagarrana/.zprofile"
+ZPROFILE_SOURCE="/Users/sagarrana/.zprofile"
+ZPROFILE_DEST="/Users/sagarrana/Documents/Kaseya_System/.zprofile"
 
-SSH_SOURCE="/Users/sagarrana/Documents/Kaseya_System/.ssh"
-SSH_DEST="/Users/sagarrana/.ssh"
+SSH_SOURCE="/Users/sagarrana/.ssh"
+SSH_DEST="/Users/sagarrana/Documents/Kaseya_System/.ssh"
 
-LOG_FILE="/Users/sagarrana/.track_and_copy.log"
+LOG_FILE="/Users/sagarrana/.track_and_copy_back.log"
 
 echo "$(date): Script started" >> "$LOG_FILE"
 
 # Copy .zprofile if it has changed
 if [ -f "$ZPROFILE_SOURCE" ]; then
     rsync -u "$ZPROFILE_SOURCE" "$ZPROFILE_DEST" >> "$LOG_FILE" 2>&1
-    echo "$(date): Synced .zprofile" >> "$LOG_FILE"
+    chown sagarrana:staff "$ZPROFILE_DEST"
+    echo "$(date): Synced and changed ownership of .zprofile" >> "$LOG_FILE"
 else
     echo "$(date): .zprofile not found" >> "$LOG_FILE"
 fi
@@ -22,7 +23,8 @@ fi
 # Sync .ssh folder
 if [ -d "$SSH_SOURCE" ]; then
     rsync -a --delete "$SSH_SOURCE/" "$SSH_DEST/" >> "$LOG_FILE" 2>&1
-    echo "$(date): Synced .ssh" >> "$LOG_FILE"
+    chown -R sagarrana:staff "$SSH_DEST"
+    echo "$(date): Synced and changed ownership of .ssh" >> "$LOG_FILE"
 else
     echo "$(date): .ssh directory not found" >> "$LOG_FILE"
 fi
